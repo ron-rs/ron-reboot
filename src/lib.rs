a0d3feca2dc0d0204a1fabf1c5bf4e791d3f0f75
Thomas Schaller <me@torkleyy.com>
2021-10-23 20:44:16 +0200

    Value with serde impls

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #[cfg(feature = "utf8_parser_serde1")]
 pub use self::utf8_parser::from_str;
+#[cfg(feature = "value")]
+pub use self::value::Value;
 pub use self::{
     error::{print_error, Error},
     location::Location,
@@ -11,3 +13,5 @@ mod location;
 #[cfg(feature = "utf8_parser")]
 pub mod utf8_parser;
 mod util;
+#[cfg(feature = "value")]
+mod value;
