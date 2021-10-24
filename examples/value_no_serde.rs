7da903794f8ed00820c4654c7b483e396158a9a8
Thomas Schaller <me@torkleyy.com>
2021-10-24 11:29:52 +0200

    Add Value example without serde

---
+++ examples/value_no_serde.rs
@@ -0,0 +1,34 @@
+use ron_reboot::{Error, print_error, Value};
+
+fn app() -> Result<(), Error> {
+    let value: Value = r#"
+MyConfig (
+    accurate: Types(are: "awesome"),
+    even: ["tuples", "and", "lists"],
+    can: {
+        "be": ("told", "apart!"),
+
+        "it's": true,
+    },
+)
+    "#.parse()?;
+
+    match value {
+        Value::Struct(Some(ident), fields) => {
+            println!("Config type: {}", ident);
+
+            for (ident, value) in fields {
+                println!("{}: {:?}", ident, value)
+            }
+        }
+        _ => unimplemented!()
+    }
+
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = app() {
+        print_error(&e).unwrap();
+    }
+}
